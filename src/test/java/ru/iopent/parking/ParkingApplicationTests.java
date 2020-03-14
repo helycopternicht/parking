4d162dd89d3cb7f4bbb69d312d1442ee5ee595bc
Eugene Lazarev <helycopternicht@rambler.ru>
2020-03-14 21:10:00 +0600

    Initial commit

---
+++ src/test/java/ru/iopent/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iopent.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
